55306e5bcdb45c9715f099cdb845ab28e46f9336
grammlaman <grammlaman@gmail.com>
2019-11-21 22:41:52 +0600

    Страницы готовы, остались только слайдеры

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
-
+    let height = 0;
+    $(window).scroll(function(e){
+        if(screen.availWidth < 760){
+            height = 50;
+        }
+        else if( (screen.availWidth >= 760) && (screen.availWidth < 1200) ){
+            height = 90;
+        }
+        else if(screen.availWidth >= 1200){
+            height = 200;
+        }
+        let scrolled = $(this).scrollTop();
+        if(scrolled >= height){
+            $('#head').addClass('head-scroll');
+        }else{
+            $('#head').removeClass('head-scroll');
+        }
+        if(scrolled >= height){
+            $('#header').addClass('head-scrolled');
+        }else{
+            $('#header').removeClass('head-scrolled');
+        }
+    })
 });
