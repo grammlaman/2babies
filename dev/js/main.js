dc837209543835179ec5cb721d380db39e1e8a8b
grammlaman <grammlaman@gmail.com>
2019-11-23 01:04:21 +0600

    Остался 1 слайдер

--- dev/js/main.js
+++ dev/js/main.js
@@ -92,4 +92,51 @@ $(function(){
             }
         ]
     });
+    $('.serf-s').slick({
+        mobileFirst: true,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 300,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $("#slider-range" ).slider({
+         range: true,
+         min: 0,
+         max: 1250000,
+         values: [48000, 1250000],
+         slide: function( event, ui ) {
+             $("#amount" ).val(ui.values[ 0 ]);
+             $("#amount-1" ).val(ui.values[ 1 ]);
+         }
+    });
+    $("#amount").val( $( "#slider-range" ).slider("values", 0));
+    $("#amount-1").val( $( "#slider-range" ).slider("values", 1));
 });
