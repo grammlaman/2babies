bb5baa8efa9eea6c6a21937328c2c8f02aef0078
grammlaman <grammlaman@gmail.com>
2019-12-05 18:08:24 +0600

    Сделали переключение цвета

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,7 @@
+let prevSpan = '';
+let prevBtn = '';
+let prevSlider = '';
+let prevNav = '';
 $(function(){
     let height = 0;
     $(window).scroll(function(e){
@@ -64,34 +68,6 @@ $(function(){
             }
         ]
     });
-    $('.product-color-s').slick({
-        mobileFirst: true,
-        arrows: true,
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 750,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 450,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 300,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
     $('.serf-s').slick({
         mobileFirst: true,
         arrows: true,
@@ -127,6 +103,35 @@ $(function(){
             }
         ]
     });
+    $('.product-color-s').slick({
+        mobileFirst: true,
+        infinite: false,
+        arrows: true,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 300,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
     let slider = $("#slider-range" ).slider({
          range: true,
          min: 0,
@@ -137,9 +142,8 @@ $(function(){
              $("#amount-1" ).val(ui.values[ 1 ]);
          }
     });
-
     $( "#amount" ).on( "change", function() {
-       let arr = slider.slider( "option", "values" );
+        let arr = slider.slider( "option", "values" );
         let pos = parseInt($(this).val());
         slider.slider( "values", [pos,arr[1]] );
     });
@@ -148,17 +152,86 @@ $(function(){
         let pos = parseInt($(this).val());
         slider.slider( "values", [arr[0],pos] );
     });
-
     $("#amount").val( $( "#slider-range" ).slider("values", 0));
     $("#amount-1").val( $( "#slider-range" ).slider("values", 1));
+    let prodInput = document.querySelectorAll('.product-input-js');
+    prodInput.forEach(function(el) {
+        let prodInput = el.querySelector('input'),
+            prodMore = el.querySelector('.product-more'),
+            prodLess = el.querySelector('.product-less'),
+            cnt = parseInt(prodInput.value);
+        prodInput.onchange = function(){
+            cnt = parseInt(this.value);
+        };
+        prodMore.onclick = function(){
+            cnt+=1;
+            if((cnt <= 0)||isNaN(cnt)) cnt = 1;
+            prodInput.value = cnt;
+        };
+        prodLess.onclick= function(){
+            cnt-=1;
+            if((cnt <= 0)||isNaN(cnt)) cnt = 1;
+            prodInput.value = cnt;
+        }
 
-    $('.product-slider-main').slick({
+
+    });
+
+
+    let colorArr = [document.querySelector('#color-slide1'),
+        document.querySelector('#color-slide2'),
+        document.querySelector('#color-slide3'),
+        document.querySelector('#color-slide4'),
+        document.querySelector('#color-slide5')];
+    let cnt = 0;
+    for(let i in colorArr){
+        let key = colorArr[i];
+        key.onclick = showSlider;
+        if(cnt == 0) key.onclick();
+        cnt++;
+    };
+});
+
+
+
+
+
+function showSlider(){
+    let slider = document.querySelector(this.getAttribute('data-slider')),
+        sliderMain = slider.children[0],
+        sliderNav = slider.children[1],
+        span = document.querySelector(this.getAttribute('data-span'));
+
+    if(prevSlider == this.getAttribute('data-slider')){
+        return;
+    }
+    this.classList.add('product-color-slide-act');
+    span.classList.add('displayBlock');
+    slider.classList.add('displayBlock');
+
+    if(prevSlider){
+        let slider = document.querySelector(prevSlider),
+            sliderMain = slider.children[0],
+            sliderNav = slider.children[1],
+            span = document.querySelector(prevSpan);
+        prevBtn.classList.remove('product-color-slide-act');
+        slider.classList.remove('displayBlock');
+        span.classList.remove('displayBlock');
+
+        $(sliderMain).slick('unslick');
+        $(sliderNav).slick('unslick');
+    }
+    prevSpan = this.getAttribute('data-span');
+    prevSlider = this.getAttribute('data-slider');
+    prevBtn = this;
+
+    $(sliderMain).slick({
         infinite: true,
         mobileFirst: true,
         dots: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        asNavFor: '.product-slider-slave',
+        asNavFor: sliderNav,
         responsive: [
             {
                 breakpoint: 768,
@@ -174,37 +247,14 @@ $(function(){
             },
         ]
     });
-    $('.product-slider-slave').slick({
+    $(sliderNav).slick({
         infinite: true,
         mobileFirst: true,
         arrows: true,
         dots: false,
         focusOnSelect: true,
-        asNavFor: '.product-slider-main',
+        asNavFor: sliderMain,
         slidesToShow: 3,
         slidesToScroll: 1
     });
-});
-
- let prodInput = document.querySelectorAll('.product-input-js');
- prodInput.forEach(function(el) {
-     let prodInput = el.querySelector('input'),
-         prodMore = el.querySelector('.product-more'),
-         prodLess = el.querySelector('.product-less'),
-         cnt = parseInt(prodInput.value);
-     prodInput.onchange= function(){
-         cnt = parseInt(this.value);
-     }
-     prodMore.onclick= function(){
-         cnt+=1;
-         if((cnt <= 0)||isNaN(cnt)) cnt = 1;
-         prodInput.value = cnt;
-     }
-     prodLess.onclick= function(){
-         cnt-=1;
-         if((cnt <= 0)||isNaN(cnt)) cnt = 1;
-         prodInput.value = cnt;
-     }
-
-
- });
+}
